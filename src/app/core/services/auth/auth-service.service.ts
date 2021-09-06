9d31f659b1271cdb852e2265c6c56f335b7aef05
MrMyatNoe <thetmyatnoe223@gmail.com>
2021-09-06 21:35:56 +0630

    chore(localStroageService): add webToken in saveData

--- src/app/core/services/auth/auth-service.service.ts
+++ src/app/core/services/auth/auth-service.service.ts
@@ -7,31 +7,28 @@ import { Router } from "@angular/router";
 export class AuthServiceService {
   private webToken: string;
   private authState = false;
-  constructor(
-    private readonly _router: Router
-  ) {
-    this.webToken = localStorage.getItem('webToken')
-    this.authState = this.webToken ? true : false 
-    console.log('set constructor', this.webToken, " : ", this.authState)
- 
+  constructor(private readonly _router: Router) {
+    this.webToken = localStorage.getItem("webToken");
+    this.authState = this.webToken ? true : false;
+    console.log("set constructor", this.webToken, " : ", this.authState);
   }
 
   setAuth(token: string) {
-    console.log('set auth', this.webToken)
+    console.log("set auth", this.webToken);
     this.webToken = token;
   }
 
   get getToken() {
-    console.log('get token', this.webToken)
+    console.log("get token", this.webToken);
     return this.webToken;
   }
 
   logout() {
     this.webToken = "";
-    this.authState = false
-    localStorage.removeItem('webToken');
-    localStorage.removeItem('driverData')
-    localStorage.removeItem('adminData')
-    this._router.navigateByUrl("/login")
-}
+    this.authState = false;
+    localStorage.removeItem("role");
+    localStorage.removeItem("driverData");
+    localStorage.removeItem("adminData");
+    this._router.navigateByUrl("/login");
+  }
 }
--- src/app/core/services/localStorage/local-storage.service.ts
+++ src/app/core/services/localStorage/local-storage.service.ts
@@ -7,33 +7,38 @@ export class LocalStorageService {
   constructor() {}
 
   saveAdminData(data: any) {
-    localStorage.setItem("admindata", JSON.stringify(data))
-    localStorage.setItem("role", data.roles[0])
-    localStorage.setItem("username",data.username)
+    localStorage.setItem("admindata", JSON.stringify(data));
+    localStorage.setItem("role", data.roles[0]);
+    localStorage.setItem("username", data.username);
+    localStorage.setItem("webToken", data.token);
   }
 
   saveDriverData(data: any) {
     localStorage.setItem("driverData", JSON.stringify(data));
     localStorage.setItem("role", "driver");
+    localStorage.setItem("username", data.username);
+    localStorage.setItem("webToken", data.token);
   }
 
   removeAdminData() {
-    localStorage.removeItem("admindata")
-    localStorage.removeItem("role")
-    localStorage.removeItem("username")
+    localStorage.removeItem("admindata");
+    localStorage.removeItem("role");
+    localStorage.removeItem("username");
+    localStorage.removeItem("webToken");
   }
 
   removeDriverData() {
     localStorage.removeItem("driverdata");
     localStorage.removeItem("role");
+    localStorage.removeItem("username");
+    localStorage.removeItem("webToken");
   }
 
   getItemRole() {
-    return localStorage.getItem("role")
+    return localStorage.getItem("role");
   }
 
   getItemUserName() {
-    return localStorage.getItem("username")
+    return localStorage.getItem("username");
   }
-
 }
